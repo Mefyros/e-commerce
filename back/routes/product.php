e7cae56c06fa68d9a161bce46d23af423473a110
buliotutyka <benjamin.renel@epitech.eu>
2019-06-09 02:28:38 +0200

    split route api + suppression route web

--- back/routes/product.php
+++ back/routes/product.php
@@ -13,6 +13,13 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// si besoin d'etre log pour avoir acces
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/products', function (Request $request) {
+    return [
+        'oui' => 'ceci est un test',
+    ];
 });
