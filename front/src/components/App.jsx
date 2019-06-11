d8d38c0705c5a87af608eaf35029e281ededf20a
buliotutyka <benjamin.renel@epitech.eu>
2019-06-11 13:51:43 +0200

    Ajout npm react-router + emotion + codyhouse

--- front/src/components/App.jsx
+++ front/src/components/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 // import ProductForm from './Form/ProductForm';
-// import Home from './Home/Home';
+import Home from './Home/Home';
 // import Panel from './Panel/Panel';
 // import Product from './Product/Product';
 
 export default () => {
   return (
     <div>
-      <p>Hello world !</p>
+      {/* <p>Hello world !</p> */}
       {/* <ProductForm /> */}
-      {/* <Home /> */}
+      <Home />
       {/* <Panel /> */}
       {/* <Product /> */}
     </div>
