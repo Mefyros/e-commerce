efefc402b900cb398e97f508c8e60c95fbd2fe50
Mefyros <bilal.beldjilali@epitech.eu>
2019-06-14 17:27:43 +0200

    Creation du product service

--- front/src/Service/ProductService.js
+++ front/src/Service/ProductService.js
@@ -1,8 +1,69 @@
 import Axios from 'axios';
 
 export default class ProductService {
-  static getAll()
+  static async getAll()
   {
-    
+    Axios.get("http://127.0.0.1:8000/api/products")
+      .then(res => {
+        return res;
+      })
+      .catch(err => {
+        return err;
+      });
+  }
+
+  static async getById(id)
+  {
+    Axios.get(`http://127.0.0.1:8000/api/product/${id}`)
+      .then(res => {
+        return res;
+      })
+      .catch(err => {
+        return err;
+      });
+  }
+
+  static async create(data)
+  {
+    Axios.post(`http://127.0.0.1:8000/api/product`, data, {headers: {'Content-Type': 'multipart/form-data' }})
+      .then(res => {
+        return res;
+      })
+      .catch(err => {
+        return err;
+      });
+  }
+
+  static async update(id, data)
+  {
+    Axios.put(`http://127.0.0.1:8000/api/product/${id}`, data, {headers: {'Content-Type': 'multipart/form-data' }})
+      .then(res => {
+        return res;
+      })
+      .catch(err => {
+        return err;
+      });
+  }
+
+  static async delete(id)
+  {
+    Axios.delete(`http://127.0.0.1:8000/api/product/${id}`)
+      .then(res => {
+        return res;
+      })
+      .catch(err => {
+        return err;
+      });
+  }
+
+  static async delete(id)
+  {
+    Axios.delete(`http://127.0.0.1:8000/api/product/${id}`)
+      .then(res => {
+        return res;
+      })
+      .catch(err => {
+        return err;
+      });
   }
 }
