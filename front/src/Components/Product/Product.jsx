984f3b26862a08c5e3d77d6a55d74a0dee019fa3
buliotutyka <benjamin.renel@epitech.eu>
2019-06-16 01:51:21 +0200

    ajout des donner en dynamique

--- front/src/Components/Product/Product.jsx
+++ front/src/Components/Product/Product.jsx
@@ -5,49 +5,70 @@ import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import ShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import { css } from 'emotion';
-import { root, paper, paperpay, zozio, paperprice, paperdesc, button } from './style';
+import style from './style';
+import ProductsService from '../../Service/ProductService';
 
 export default class Product extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  componentWillMount = async () => {
+    const product = await ProductsService.getById(this.props.match.params.id);
+    this.setState({ 
+      ...product.data.data,
+      photos: JSON.parse(product.data.data.photos),
+      specs: JSON.parse(product.data.data.specs),
+    });
+  }
+
   render() {
+    const { name, photos, price, quantity, specs, description } = this.state;
+
     return(
       <Container maxWidth="lg">
-        <div className={css(root)}>
+        <div className={css(style.root)}>
           <Grid container spacing={3}>
             <Grid item xs={12} md={4}>
-              <Paper className={css(paper)}>
-                <img className={css(zozio)} src="https://jardinage.lemonde.fr/images/dossiers/2017-12/pic-epeiche-1-135005.jpg" alt="oisal"/>
-                </Paper>
-              <Paper className={css(paperpay)}>42,42€ ─ 
-                <Button variant="contained" color="primary" className={css(button)}>
+
+              <Paper className={css(style.paper)}>
+                {
+                  photos !== undefined ? (
+                    <img className={css(style.zozio)} src={photos[0]} alt={name}/>
+                  ) : (
+                    null
+                  )
+                }
+              </Paper>
+
+              <Paper className={css(style.paperpay)}>
+                <h6 className={css(style.price)}>{price}€</h6>
+                <Button variant="contained" color="primary" className={css(style.button)}>
                   <ShoppingCartIcon id="add-to-cart"/>
                 </Button>
               </Paper>
-              <Paper className={css(paperprice)}>
-                <h3 style={{ color: 'red' }}>Stock: Not available</h3>
+
+              <Paper className={css(style.paperprice)}>
+                {
+                  quantity < 1 ? (
+                    <h3 className={css(style.notInStock)}>Product not available</h3>
+                  ) : quantity > 1 ? (
+                    <h3 className={css(style.inStock)}>{quantity} products available</h3>
+                  ) : (
+                    <h3 className={css(style.inStock)}>{quantity} product available</h3>
+                  )
+                }
               </Paper>
+
             </Grid>
+
             <Grid item xs={12} md={8}>
-              <Paper className={css(paper)}>Petit Oisal Gris - Gold Édition</Paper>
+              <Paper className={css(style.paper)}>{name}</Paper>
   
-              <Paper className={css(paperdesc)}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris semper 
-                odio vitae diam commodo facilisis. Pellentesque euismod, leo sed luctus hendrerit, mauris nisl pharetra dolor, 
-                eget lobortis risus lectus vitae sem. Integer iaculis ipsum id aliquam tincidunt. Quisque id dapibus velit. 
-                Phasellus et felis mattis, sollicitudin diam non, dignissim tellus. Nam ut nisl auctor, pretium tellus vel, 
-                euismod ligula. Pellentesque id maximus leo. Nunc eget odio consectetur, sollicitudin metus quis, condimentum 
-                magna. Curabitur sed congue sem. Cras a dolor vitae ante pretium pharetra. Donec tincidunt placerat magna, 
-                eget laoreet velit porta vel. Sed sollicitudin porttitor consectetur. Donec velit elit, sagittis ac tristique 
-                in, ornare in dui. Vivamus auctor pulvinar lobortis. Vivamus ullamcorper enim volutpat dolor malesuada posuere 
-                nec eu metus. Curabitur sit amet risus feugiat, ultricies lorem nec, lacinia leo. Fusce condimentum egestas dictum. 
-                Donec quis leo mollis, congue ante vel, mattis purus. Cras commodo lectus libero, non lobortis metus dapibus vel. 
-                Duis imperdiet elit at tortor efficitur efficitur. Cras eu mauris volutpat, efficitur lacus ac, finibus neque. 
-                Integer ligula risus, pretium ultricies ullamcorper eget, pellentesque at ipsum. Duis ultrices justo a mi blandit, 
-                nec convallis odio dapibus. Cras interdum ultricies velit, sit amet luctus nisi porttitor sit amet. Nam sed 
-                iaculis neque. Pellentesque turpis nulla, pulvinar at scelerisque a, gravida vel est. Morbi imperdiet et sem 
-                non rhoncus. Aenean lectus lacus, accumsan id pellentesque non, feugiat a leo. Duis iaculis urna turpis, 
-                id ornare lectus interdum in. Integer finibus facilisis neque, imperdiet luctus mauris ultricies quis. 
-              </Paper>
+              <Paper className={css(style.paperdesc)}>{description}</Paper>
             </Grid>
+
           </Grid>
         </div>
       </Container>
--- front/src/Components/Product/style.js
+++ front/src/Components/Product/style.js
@@ -1,54 +1,72 @@
 import { css } from '@emotion/core';
 import Color from '../Color';
 
+export default {
+  root: css`
+    flex-grow: 1;
+  `,
 
-export const root = css`
-  flex-grow: 1;
-`;
+  paper: css`
+    padding: .5em;
+    text-align: center;
+    color: ${Color.lightGrey};
+  `,
 
-export const paper = css`
-  padding: .5em;
-  text-align: center;
-  color: ${Color.lightGrey};
-`;
+  zozio: css`
+    width: 100%;
+    height: 100%;
+  `,
 
-export const zozio = css`
-  width: 100%;
-  height: 100%;
-`;
+  paperpay: css`
+    margin-top: 50px;
+    font-size: 39px;
+    padding: .5em;
+    text-align: center;
+    color: ${Color.lightGrey};
+    display: flex;
+    align-content: center;
+    justify-content: center;
+  `,
 
-export const paperpay = css`
-  margin-top: 50px;
-  font-size: 39px;
-  padding: .5em;
-  text-align: center;
-  color: ${Color.lightGrey};
-`;
+  paperdesc: css`
+    margin-top: 15px;
+    padding: .5em;
+    text-align: center;
+    color: ${Color.lightGrey};
+  `,
 
-export const paperdesc = css`
-  margin-top: 15px;
-  padding: .5em;
-  text-align: center;
-  color: ${Color.lightGrey};
-`;
+  paperprice: css`
+    margin-top: 34px;
+    color: red;
+    padding: .5em;
+    text-align: center;
+  `,
 
-export const paperprice = css`
-  margin-top: 34px;
-  color: red;
-  padding: .5em;
-  text-align: center;
-`;
+  button: css`
+    background-color: ${Color.madForMango} !important;
+    border: solid 1px ${Color.lightGrey} !important;
+    border-radius: 0px !important;
+    box-shadow: -1px -1px 5px 1px rgba(0,0,0,.05), 1px 1px 5px 1px rgba(0,0,0,.01) !important;
+    padding: .7em 1.1em !important;
+    margin-left: 2em !important;
 
-export const button = css`
-  background-color: ${Color.madForMango} !important;
-  border: solid 1px ${Color.lightGrey} !important;
-  border-radius: 3px !important;
-  box-shadow: -1px -1px 5px 1px rgba(0,0,0,.05), 1px 1px 5px 1px rgba(0,0,0,.01) !important;
-  padding: .7em 1.1em !important;
+    &:hover {
+      cursor: pointer;
+      background-color: ${Color.madForMangoHover} !important;
+      box-shadow: -1px -1px 5px 1px rgba(0,0,0,.05), 1px 1px 5px 1px rgba(0,0,0,.1) !important;
+    }
+  `,
 
-  &:hover {
-    cursor: pointer;
-    background-color: ${Color.madForMangoHover} !important;
-    box-shadow: -1px -1px 5px 1px rgba(0,0,0,.05), 1px 1px 5px 1px rgba(0,0,0,.1) !important;
-  }
-`;
+  inStock: css`
+    color: ${Color.green};
+  `,
+
+  notInStock: css`
+    color: ${Color.red};
+  `,
+
+  price: css`
+    font-size: 1em;
+    margin: 0;
+  `,
+}
