dd70e0efd1ac323ca475c8715ae1132b5bffe351
Benjamin RENEL <4006297-bubliotutyka@users.noreply.gitlab.com>
2019-06-17 10:52:54 +0000

    Revert "Merge branch 'front/navbar' into 'master'"
    This reverts merge request !43

--- front/src/Components/Navbar/Navbar.jsx
+++ front/src/Components/Navbar/Navbar.jsx
@@ -1,37 +1,13 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import TextField from '@material-ui/core/TextField'
-import FormHelperText from '@material-ui/core/FormHelperText';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { css }  from 'emotion';
-import { header, button, searchbar, buttonend, searchInput, searchChildren, searchSubmit, searchSelect } from './style';
-
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import FormControl from '@material-ui/core/FormControl';
-import SearchIcon from '@material-ui/icons/Search';
-import UserIcon from '@material-ui/icons/Person';
-import IconButton from '@material-ui/core/IconButton';
-
-import CategoryService from '../../Service/CategoryService.js';
+import { header, button } from './style';
 
 export default class Navbar extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      categorie_list: [],
-      input_searc: '',
-    }
-  }
-  async componentDidMount(){
-    var categories = await CategoryService.getAll();
-    this.setState({categorie_list: categories});
-  }
 
   render() {
     return (
@@ -43,29 +19,6 @@ export default class Navbar extends React.Component {
           <Typography variant="h6" color="inherit">
             <Button className={css(button)} component={Link} color="inherit" to="/panel">Panel</Button>
           </Typography>
-        <div className={css(searchbar)}>
-          <div className={css(searchChildren)}>
-            <select value='hey' className={css(searchSelect)}>
-            {
-              this.state.categorie_list.map((categorie) => <option value={categorie.id}>{categorie.name}</option>)
-            }
-            </select>
-            <input placeholder={'Search...'} className={css(searchInput)}type="text"/>
-            <button className={css(searchSubmit)}><SearchIcon/></button>
-          </div>
-        </div>
-            <div className={css(buttonend)}>
-            <Button component={Link} color="inherit" to="/">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M17.21 9l-4.38-6.56c-.19-.28-.51-.42-.83-.42-.32 0-.64.14-.83.43L6.79 9H2c-.55 0-1 .45-1 1 0 .09.01.18.04.27l2.54 9.27c.23.84 1 1.46 1.92 1.46h13c.92 0 1.69-.62 1.93-1.46l2.54-9.27L23 10c0-.55-.45-1-1-1h-4.79zM9 9l3-4.4L15 9H9zm3 8c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z"/></svg>
-            <FormHelperText>Cart</FormHelperText>
-            </Button>
-
-            <Button component={Link} color="inherit" to="/">
-            <UserIcon />
-            <FormHelperText>User</FormHelperText>
-            </Button>
-            </div>
-
         </Toolbar>
       </AppBar>
     );
