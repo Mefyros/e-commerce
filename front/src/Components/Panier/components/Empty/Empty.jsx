deaecde85b6297aaf11e973b7bb5fedc481e5793
Azumi <jade.perianayagassamy@epitech.eu>
2019-06-18 17:02:40 +0200

    base wip

---
+++ front/src/Components/Panier/components/Empty/Empty.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default class EmptyCard extends React.Component {
+
+
+    render() {
+
+        return (
+            <div>
+                <h1>Oh No! Your basket is empty</h1>
+            </div>
+        );
+    }
+}
---
+++ front/src/Components/Panier/components/Full/Full.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import BasketCard from './components/card';
+
+export default class FullCard extends React.Component {
+
+
+    render() {
+
+        return (
+            <div>
+                <BasketCard/>
+            </div>
+
+        );
+    }
+}
