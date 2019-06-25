52548a120b4fea4576682c0c5b60221f3cae48d5
buliotutyka <benjamin.renel@epitech.eu>
2019-06-25 23:35:49 +0200

    Breadcrumds v2

--- front/src/Components/Breadcrumbs/Breadcrumbs.jsx
+++ front/src/Components/Breadcrumbs/Breadcrumbs.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { css } from 'emotion';
 import style, {
   Container,
+  BreackContainer,
   BreadLink,
   BreadLinkHome,
   Arrow,
@@ -10,20 +11,22 @@ import style, {
 export default (props) => {
   return (
     <Container>
-      <BreadLinkHome href="/">
-        <i className={`fas fa-home ${css(style.homeIcon)}`}></i>
-        Home
-        <Arrow />
-      </BreadLinkHome>
+      <BreackContainer>
+        <BreadLinkHome href="/">
+          <i className={`fas fa-home ${css(style.homeIcon)}`}></i>
+          Home
+          <Arrow />
+        </BreadLinkHome>
 
-      {
-        props.links.map((link, key) => (
-          <BreadLink key={key} href={link.url} >
-            {link.name}
-            <Arrow />
-          </BreadLink>
-        ))
-      }
+        {
+          props.links.map((link, key) => (
+            <BreadLink key={key} href={link.url} >
+              {link.name}
+              <Arrow />
+            </BreadLink>
+          ))
+        }
+      </BreackContainer>
     </Container>
   );
 }
--- front/src/Components/Breadcrumbs/style.js
+++ front/src/Components/Breadcrumbs/style.js
@@ -17,9 +17,8 @@ const defaultLink = css`
   display: flex;
   color: ${Color.white};
   background-color: ${Color.ecoGreen};
-  padding: .7em 1em .7em 0;
+  padding: .4em 1em .4em 0;
   font-weight: 400;
-  /* box-shadow: ${Color.hoverShadow}; */
   font-size: 1em;
 
   &:hover {
@@ -33,14 +32,17 @@ const defaultLink = css`
   }
 `;
 
-export const Container = styled.div`
-  height: 50px;
-  margin: 0;
-  margin-bottom: 2em;
+export const BreackContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: start;
   align-items: center;
+  border-radius: .7em;
+`;
+
+export const Container = styled.div`
+  margin: 0;
+  margin-bottom: 2em;
 
   @media (max-width: 768px) {
     visibility: hidden;
@@ -52,11 +54,21 @@ export const BreadLinkHome = styled.a`
   ${defaultLink}
   padding-left: 1em;
   border-top-left-radius: ${borderRadius};
-  border-bottom-left-radius: ${borderRadius};
+  border-bottom-left-radius: ${borderRadius};  
+  /* box-shadow: -5px -5px 5px 1px rgba(0,0,0,.05),
+              -5px 5px 5px 1px rgba(0,0,0,.05),
+              3px 5px 5px 1px rgba(0,0,0,.05),
+              3px -5px 5px 1px rgba(0,0,0,.05); */
+
+  i.fas {
+    margin-bottom: 4px;
+  }
 `;
 
 export const BreadLink = styled.a`
   ${defaultLink}
+  /* box-shadow: 5px -5px 5px 1px rgba(0,0,0,.1),
+              5px 5px 5px 1px rgba(0,0,0,.1); */
 
   &:last-child {
     border-top-right-radius: ${borderRadius};
@@ -65,18 +77,16 @@ export const BreadLink = styled.a`
 
     i {
       visibility: hidden;
-      position: absolute;
     }
   }
 `;
 
 export const Arrow = styled.i`
   width: 25px;
+  height: 25px;
   transform: rotate(45deg);
   background-color: transparent;
-  /* box-shadow: 0 1px 5px 0px rgba(0,0,0,.2); */
-  border-top: 1px rgba(0,0,0,.1) solid;
-  border-right: 1px rgba(0,0,0,.1) solid;
-  box-shadow: 11px 0px 8px -10px rgba(0,0,0,.2),
-              0px -11px 8px -10px rgba(0,0,0,.2); 
+  border-top: 1px rgba(0,0,0,.15) solid;
+  border-right: 1px rgba(0,0,0,.15) solid;
+  box-shadow: 5px -5px 10px rgba(0,0,0,.1);
 `;
