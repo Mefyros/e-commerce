bceb77fd0dc7673901f5a1bbe96043c77693e52f
buliotutyka <benjamin.renel@epitech.eu>
2019-06-25 17:23:51 +0200

    breadcrumbs v1

--- front/src/Components/Breadcrumbs/style.js
+++ front/src/Components/Breadcrumbs/style.js
@@ -16,9 +16,8 @@ const borderRadius = '.7em';
 const defaultLink = css`
   display: flex;
   color: ${Color.white};
-  padding: 0 .5em;
   background-color: ${Color.ecoGreen};
-  padding: .7em 1em;
+  padding: .7em 1em .7em 0;
   font-weight: 400;
   /* box-shadow: ${Color.hoverShadow}; */
   font-size: 1em;
@@ -51,6 +50,7 @@ export const Container = styled.div`
 
 export const BreadLinkHome = styled.a`
   ${defaultLink}
+  padding-left: 1em;
   border-top-left-radius: ${borderRadius};
   border-bottom-left-radius: ${borderRadius};
 `;
@@ -71,13 +71,12 @@ export const BreadLink = styled.a`
 `;
 
 export const Arrow = styled.i`
-  /* height: 32px; */
-  width: 100%;
+  width: 25px;
   transform: rotate(45deg);
   background-color: transparent;
   /* box-shadow: 0 1px 5px 0px rgba(0,0,0,.2); */
-  border-top: 1px rgba(0,0,0,.2) solid;
-  border-right: 1px rgba(0,0,0,.2) solid;
+  border-top: 1px rgba(0,0,0,.1) solid;
+  border-right: 1px rgba(0,0,0,.1) solid;
   box-shadow: 11px 0px 8px -10px rgba(0,0,0,.2),
               0px -11px 8px -10px rgba(0,0,0,.2); 
 `;
